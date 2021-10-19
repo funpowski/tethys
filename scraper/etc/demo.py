d4225b890fd78ed8c038675d7e08bc92df30331d
riomcmahon <48159564+rmcsqrd@users.noreply.github.com>
2021-10-18 21:10:00 -0600

    added in demo file with basic functionality for scraping data into df, then reading/writing df into postgres container via pandas

--- scraper/etc/demo.py
+++ scraper/etc/demo.py
@@ -1,10 +1,19 @@
-import yaml
+import pandas as pd
+from sqlalchemy import create_engine  
+from sqlalchemy import Table, Column, String, MetaData
 import tethysio
-import pickle
-import socket
+import yaml
+
+# define config stuff based on docker-compose
+PG_PASSWORD = 'password'
+PG_USER = 'admin'
+PG_DB = 'postgres'
+PG_HOST = 'db' # same as container name
 
 if __name__ == '__main__':
-    with open("scraper_config.yaml", "r") as f:
+
+    # first scrape the data
+    with open("/config/scraper_config.yaml", "r") as f:
         conf = yaml.safe_load(f)
 
     h = conf['headers']
@@ -15,7 +24,14 @@ if __name__ == '__main__':
 
     df = r.api_request(permit_id, division_id, start_date, end_date)
 
-    #msg = pickle.dumps(df)  # BONE eventually use this for socket io
-    df.to_pickle("testdata.pkl")
+    # next, send data to db
+    engine = create_engine(f'postgresql+psycopg2://{PG_USER}:{PG_PASSWORD}@{PG_HOST}/{PG_DB}')
+    try:
+        df.to_sql("test", engine)
+    except:
+        pass  # just incase "test" exists already
 
+    # finally, pull from db
+    sql_df = pd.read_sql("test", engine)
+    print(sql_df)
 
